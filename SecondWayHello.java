fa6e3d1e0fa7725c9c60a7f3005c68903b63d35a
vasundhara1224 <56587157+vasundhara1224@users.noreply.github.com>
2019-11-26 15:06:06 +0530

    Add files via upload

---
+++ SecondWayHello.java
@@ -0,0 +1,20 @@
+public class SecondWayHello
+{
+	//changing sequence of modifier,method  prototype is not changed in java//
+ static public void main(String args[])
+{
+
+System.out.println("HelloWorld");
+}
+}
+//different ways of valid java main method sigature
+
+ //==> public static void main(String args[])
+//==>  public static void main(String []args)
+//==>  public static void main(String[] args)
+//==> public static void main(String... args)
+//==>   static public void main(String args[])
+//==>  public static final void main(String[] args)
+//==>   final public static void main(String[] args)
+//==> final strictfp public static void main(String[] args)
+//any one of these can be used.//
---
+++ ThirdWayHello.java
@@ -0,0 +1,10 @@
+//by using Strings//
+public class ThirdWayHello{
+	 public static void main(String[] args)
+	  {
+	 	String s1="HelloWorld";//creating string by using String literal
+	 	System.out.println(s1+" using String literal");
+	 	String s2=new String("hello");//creating string by using new keyword
+	System.out.println(s2+" using new keyword");
+}
+}
